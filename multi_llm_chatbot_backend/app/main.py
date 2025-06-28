093722a8f3c103699b3e52f49c9314a606d767e4
chandanasunkara <chandana.sunkara@colorado.edu>
2025-06-28 17:19:36 -0400

    BACKEND : Setup Database to store and retrieve information

--- multi_llm_chatbot_backend/app/main.py
+++ multi_llm_chatbot_backend/app/main.py
@@ -1,7 +1,7 @@
 # app/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.routes import router
+from app.api.routes import router as api_router
 from dotenv import load_dotenv
 import os
 
@@ -23,7 +23,8 @@ app.add_middleware(
 )
 
 # Include route definitions
-app.include_router(router)
+# app.include_router(router)
+app.include_router(api_router, prefix="/api")
 
 @app.get("/")
 def root():
