9ef7c3f82884dc3f29c35bd3c87a524667ac7bc9
chandanasunkara <chandana.sunkara@colorado.edu>
2025-07-02 22:16:12 -0400

    Embedding changes

--- multi_llm_chatbot_backend/app/llm/embedding_client.py
+++ multi_llm_chatbot_backend/app/llm/embedding_client.py
@@ -1,7 +1,5 @@
 from sentence_transformers import SentenceTransformer
 import os
-import httpx
-import hashlib
 
 API_KEY = os.getenv("GEMINI_API_KEY")
 
@@ -9,6 +7,5 @@ API_KEY = os.getenv("GEMINI_API_KEY")
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def get_embedding(text: str) -> list[float]:
-    # Use deterministic hash → convert to float vector
-    hashed = hashlib.sha256(text.encode()).hexdigest()
-    return [int(hashed[i:i+4], 16) / 65536.0 for i in range(0, len(hashed), 4)]
+    embedding = model.encode(text, convert_to_numpy=True)
+    return embedding.tolist()
